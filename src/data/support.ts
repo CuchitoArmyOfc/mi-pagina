30b4ef4f4b54665cd28430065b2d0100d956af91
CuchitoArmyOfc <164554999+CuchitoArmyOfc@users.noreply.github.com>
2026-08-16 17:24:13 -0500

    Delete src directory

--- src/data/support.ts
+++
@@ -1,7 +0,0 @@
-export const support = {
-  name: 'FantasMindox',
-  role: 'Desarrollador oficial de Cuchito Army',
-  description: '¿Necesitas soporte técnico o quieres sugerir una mejora? Escríbeme directamente.',
-  href: 'https://t.me/fantasmindox',
-  cta: 'Contactar por Telegram',
-};
