cc3d47183bc6ad38aabb8d985acdce3254ae5ca7
CuchitoArmyOfc <164554999+CuchitoArmyOfc@users.noreply.github.com>
2026-08-17 00:46:35 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.cuchitoarmyoficial.com',
   trailingSlash: 'never',
   compressHTML: true,
+  integrations: [sitemap()],
   build: {
     inlineStylesheets: 'auto',
   },
